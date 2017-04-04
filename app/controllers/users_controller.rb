6abf34dedd51a241d7e45b41061f7a53f95659da
Joseph Surla <josephsurla@gmail.com>
2017-04-04 09:10:04 -0700

    Add working signup page and redirects to users homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
 
+  get '/users/index' do
+    erb :'/users/index'
+  end
+
   get '/signup' do
-    erb :'signup'
+    erb :'/users/signup'
+  end
+
+  post '/signup' do
+    @user = User.new(email: params[:email], password: params[:password])
+    @user.save
+    erb :'/users/index'
   end
 end
