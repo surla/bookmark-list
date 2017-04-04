5ff896a61cce7ab405480673588ccef73c794cbc
Joseph Surla <josephsurla@gmail.com>
2017-04-04 09:35:53 -0700

    Fix error in post /login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,12 +23,17 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect '/users/index'
     else
       redirect '/signup'
     end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
   end
 end
