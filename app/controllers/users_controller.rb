ce0d6a8a430473dd0a208f42921872638a3ef4fe
Joseph Surla <josephsurla@gmail.com>
2017-04-04 09:26:04 -0700

    Update post /signup to save user.id in session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,25 @@ class UsersController < ApplicationController
 
   post '/signup' do
     @user = User.new(email: params[:email], password: params[:password])
-    @user.save
-    erb :'/users/index'
+    if @user.save
+      session[:user_id] = @user.id
+      erb :'/users/index'
+    else
+      redirect '/login'
+    end
   end
 
   get '/login' do
     erb :'/users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/users/index'
+    else
+      redirect '/signup'
+    end
   end
 end
