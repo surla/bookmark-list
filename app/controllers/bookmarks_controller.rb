fb159ed6704fffcf50235197f8d1e25f25d2a90b
Joseph Surla <josephsurla@gmail.com>
2017-04-04 22:14:41 -0700

    Create routes for creating new bookmark. Add index to view all bookmarks.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,17 @@
 class BookmarksController < ApplicationController
 
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'/bookmarks/index'
+  end
+
   get '/bookmarks/new' do
     erb :'/bookmarks/new'
+  end
+
+  post '/bookmarks/new' do
+    @bookmark = Bookmark.new(url: params[:url], title: params[:title], description: params[:description])
+    @bookmark.save
+    redirect '/bookmarks'
   end
 end
