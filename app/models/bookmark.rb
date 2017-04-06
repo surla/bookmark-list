579c5183825a2c19675e7e4fbcff8e2bd9ddbfa3
Joseph Surla <josephsurla@gmail.com>
2017-04-06 09:09:51 -0700

    Create has_many and belongs_to association between user and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,3 @@
 class Bookmark < ActiveRecord::Base
-  
+  belongs_to :user
 end
---
+++ db/migrate/20170406153619_add_bookmark_to_user.rb
@@ -0,0 +1,5 @@
+class AddBookmarkToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :bookmarks, :user_id, :integer
+  end
+end
