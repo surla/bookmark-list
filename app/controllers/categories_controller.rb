336b5bf03c05dd1303a877a14432098e615e654d
Joseph Surla <josephsurla@gmail.com>
2017-04-11 22:34:43 -0700

    Create index route and view to display all categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,8 @@
-class Categories < ApplicationController
+class CategoriesController < ApplicationController
+
+  get '/categories' do
+    @categories = Category.all
+    erb :'/categories/index'
+  end
 
 end
--- config.ru
+++ config.ru
@@ -7,4 +7,5 @@ end
 use Rack::MethodOverride
 use UsersController
 use BookmarksController
+use CategoriesController
 run ApplicationController
