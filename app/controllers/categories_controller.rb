3e2f2170e401cce1971a5090d7f05b66449ca19c
Joseph Surla <josephsurla@gmail.com>
2017-04-12 14:02:10 -0700

    Create show action and view to display bookmarks for a particular category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,8 @@ class CategoriesController < ApplicationController
     erb :'/categories/index'
   end
 
+  get '/categories/:id' do
+    @category = Category.find_by_id(params[:id])
+    erb :'/categories/show'
+  end
 end
